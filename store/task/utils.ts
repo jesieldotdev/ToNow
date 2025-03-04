025d72880e5208e288c78debb185d6239bc957fa
jesieldotdev <jesiel364@gmail.com>
2025-03-04 03:21:59 -0400

    feat: sortTasksByNearestTime

--- store/task/utils.ts
+++ store/task/utils.ts
@@ -1,40 +1,66 @@
 export const getDefaultTaskItem = (): TaskItem => {
-    const now = new Date();
+  const now = new Date();
 
-    return {
-        title: '',
-        description: '',
-        time: {
-            hour: now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" }),
-            date: {
-                day: { label: now.toLocaleDateString('en-US', { weekday: 'short' }), day: now.getDate() },
-                month: { label: now.toLocaleDateString('en-US', { month: 'short' }), day: now.getMonth() + 1 },
-                year: now.getFullYear(),
-                dayWeek: { value: now.getDay(), label: now.toLocaleDateString('en-US', { weekday: 'short' }) }
-            }
-        },
-    };
+  return {
+    title: '',
+    description: '',
+    time: {
+      hour: now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" }),
+      date: {
+        day: { label: now.toLocaleDateString('en-US', { weekday: 'short' }), day: now.getDate() },
+        month: { label: now.toLocaleDateString('en-US', { month: 'short' }), day: now.getMonth() + 1 },
+        year: now.getFullYear(),
+        dayWeek: { value: now.getDay(), label: now.toLocaleDateString('en-US', { weekday: 'short' }) }
+      }
+    },
+  };
 };
 
 export const sortTasks = (tasks: TaskItem[]): TaskItem[] => {
-    return [...tasks].sort((a, b) => {
-      const dateA = new Date(
-        a.time.date.year,
-        a.time.date.month.day - 1, // Mês começa do 0 no JS
-        a.time.date.day.day,
-        parseInt(a.time.hour.split(":")[0]), // Hora
-        parseInt(a.time.hour.split(":")[1]) // Minuto
+  return [...tasks].sort((a, b) => {
+    const dateA = new Date(
+      a.time.date.year,
+      a.time.date.month.day - 1, 
+      a.time.date.day.day,
+      parseInt(a.time.hour.split(":")[0]), 
+      parseInt(a.time.hour.split(":")[1]) 
+    );
+
+    const dateB = new Date(
+      b.time.date.year,
+      b.time.date.month.day - 1,
+      b.time.date.day.day,
+      parseInt(b.time.hour.split(":")[0]),
+      parseInt(b.time.hour.split(":")[1])
+    );
+
+    return dateA.getTime() - dateB.getTime(); 
+  });
+};
+
+export const sortTasksByNearestTime = (tasks: TaskItem[]): TaskItem[] => {
+  return [...tasks]
+    .map((task) => {
+      const taskDate = new Date(
+        task.time.date.year,
+        task.time.date.month.day - 1, 
+        task.time.date.day.day,
+        parseInt(task.time.hour.split(":")[0]), 
+        parseInt(task.time.hour.split(":")[1]) 
       );
-  
-      const dateB = new Date(
-        b.time.date.year,
-        b.time.date.month.day - 1,
-        b.time.date.day.day,
-        parseInt(b.time.hour.split(":")[0]),
-        parseInt(b.time.hour.split(":")[1])
-      );
-  
-      return dateA.getTime() - dateB.getTime(); // Ordena de forma ascendente
+
+      return {
+        ...task,
+        timestamp: taskDate.getTime(), 
+      };
+    })
+    .sort((a, b) => {
+      const now = Date.now();
+      const diffA = Math.abs(a.timestamp - now); 
+      const diffB = Math.abs(b.timestamp - now);
+
+      return diffA - diffB; 
     });
-  };
-  
+};
+
+
